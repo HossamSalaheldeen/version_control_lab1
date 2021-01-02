eee0e4872e1b38cb9c58becd2c2e42f062339877
Khaled ElGaml <khaledelgaml6@gmail.com>
2021-01-02 10:23:12 -0800

    added history to file

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,12 @@
-cdconsole.log("hello from khaled");
+console.log("hello from khaled");
+
+console.log("PS C:\Users\khale\OneDrive\Desktop\first lab\version_control_lab1> git log
+        commit 7 b72a158cb7bb20bb2a56ede789f40c8a73b5916(HEAD - > main, origin / main, origin / HEAD) Author: Khaled ElGaml < khaledelgaml6 @gmail.com >
+        Date: Sat Jan 2 10: 16: 32 2021 - 0800
+
+        i changed your js file
+
+        commit 15 b5c73d24afc16e763c6961e651a13ca32d2069 Author: HossamSalaheldeen < hossam.salah1995 @gmail.com >
+        Date: Sat Jan 2 09: 44: 51 2021 + 0200
+
+        committing helloworld.js in master: ");
